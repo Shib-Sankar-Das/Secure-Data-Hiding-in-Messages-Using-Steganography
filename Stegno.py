036040c4318a0b18baf24a1acd273477211ebee4
Shib Sankar Das <136646947+Shib-Sankar-Das@users.noreply.github.com>
2025-02-22 10:39:56 +0530

    Update Stegno.py

--- Stegno.py
+++ Stegno.py
@@ -140,7 +140,7 @@ elif app_mode == "ENCODE MESSAGE":
     uploaded_image = st.file_uploader("📂 Upload Cover Image", type=["png", "jpg", "jpeg"])
     if uploaded_image:
         image = Image.open(uploaded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
         input_type = st.radio("Select Input Type", ["✏ Enter Text", "📄 Upload Text File"])
 
@@ -183,7 +183,7 @@ elif app_mode == "DECODE MESSAGE":
     uploaded_encoded_image = st.file_uploader("📂 Upload the Encoded Image", type=["png", "jpg", "jpeg"])
     if uploaded_encoded_image:
         image = Image.open(uploaded_encoded_image)
-        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.image(image, caption="Uploaded Image", use_container_width=True)
 
     password = st.text_input("🔑 Enter Password for Decryption", type="password")
 
